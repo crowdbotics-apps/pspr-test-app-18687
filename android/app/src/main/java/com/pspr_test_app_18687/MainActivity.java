566a467007e4601e81b9ef96c90ee3550eb1c8ee
Crowdbotics <team@crowdbotics.com>
2020-07-07 23:07:02 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/pspr_test_app_18687/MainActivity.java
@@ -0,0 +1,15 @@
+package com.pspr_test_app_18687;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+     * Returns the name of the main component registered from JavaScript.
+     * This is used to schedule rendering of the component.
+     */
+    @Override
+    protected String getMainComponentName() {
+        return "pspr_test_app_18687";
+    }
+}
